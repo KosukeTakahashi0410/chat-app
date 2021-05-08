be7146605584c66f94053e074a196286f0a54261
takahashikosuke <takohashi0410@gmail.com>
2021-05-08 13:15:33 +0900

    [add]Successページ（ユーザー登録ボタン押下後に遷移するページ）を追加

---
+++ src/pages/Success.js
@@ -0,0 +1,12 @@
+const Success = () => {
+  // 閉じるボタンは実装できなさそうなので却下
+  // TODO　Takahashi 戻るのときにフォームへ遷移できないようにする、多分routerの機能であるはず
+  return (
+    <div>
+      <p>Your Signup almost done.</p>
+      <p>Check your email and confirm your acount!</p>
+    </div>
+  );
+}
+
+export default Success
